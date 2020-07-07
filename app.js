ee51e017bfcd76b46498f3fb49347f3ecc74b33d
Subham Shrestha <subham.hero01@gmail.com>
2020-07-07 08:12:04 +0300

    added the user account info

---
+++ app.js
@@ -0,0 +1,50 @@
+function setupGuides(docs) {
+  let html = "";
+  docs.forEach((doc) => {
+    let li = `
+    <li>
+    <div class="collapsible-header grey lighten-4">${doc.data().title}</div>
+  <div class="collapsible-body white">
+    <span>${doc.data().content}</span>
+  </div>
+    </li>
+    `;
+    html += li;
+  });
+  let ul = document.querySelector("ul.guides");
+  ul.innerHTML = html;
+}
+
+let loggedInLinks = document.querySelectorAll(".logged-in");
+let loggedOutLinks = document.querySelectorAll(".logged-out");
+let accdetails = document.querySelector(".account-details");
+function toggleNav(user) {
+  if (user) {
+    loggedInLinks.forEach((item) => {
+      item.style.display = "block";
+      const html = `
+      <h6>Logged in as ${user.email}</h6>
+      `;
+      accdetails.innerHTML = html;
+    });
+    loggedOutLinks.forEach((item) => {
+      item.style.display = "none";
+    });
+  } else {
+    loggedInLinks.forEach((item) => {
+      item.style.display = "none";
+    });
+    loggedOutLinks.forEach((item) => {
+      item.style.display = "block";
+    });
+    accdetails.innerHTML = "";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
+});
---
+++ auth.js
@@ -0,0 +1,94 @@
+//tracking auth status
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    //database get
+    db.collection("guides").onSnapshot(
+      (snapshot) => {
+        setupGuides(snapshot.docs);
+      },
+      (err) => {
+        console.log(err.message);
+      }
+    );
+  } else {
+    let ul = document.querySelector("ul.guides");
+    ul.innerHTML = `<h5 class="center-align">Please Log In to view the documents</h5>`;
+  }
+  toggleNav(user);
+});
+
+//toggling loading functions
+const progressBar = document.querySelectorAll(".pb");
+function startSpin() {
+  progressBar.forEach((item) => {
+    item.classList.add("progress");
+  });
+}
+function stopSpin() {
+  progressBar.forEach((item) => {
+    item.classList.remove("progress");
+  });
+}
+
+//create a new account
+
+const signupForm = document.querySelector("#signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+  startSpin();
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    // console.log(cred.user);
+
+    stopSpin();
+    const modal = document.querySelector("#modal-signup");
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+//logging out
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+//signing users in
+
+const loginForm = document.querySelector("#login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+  startSpin();
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    stopSpin();
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
+
+//create form
+
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  startSpin();
+  db.collection("guides")
+    .add({
+      title: createForm["title"].value,
+      content: createForm["content"].value,
+    })
+    .then((res) => {
+      stopSpin();
+      createForm.reset();
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
