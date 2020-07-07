5171297354b755b3a23ca864e55abd78578489d8
Subham Shrestha <subham.hero01@gmail.com>
2020-07-07 21:57:09 +0300

    getting user database was in forEach function somehow..now its outside

--- app.js
+++ app.js
@@ -22,20 +22,21 @@ function toggleNav(user) {
   if (user) {
     loggedInLinks.forEach((item) => {
       item.style.display = "block";
-      db.collection("users")
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          const html = `
-        <h6>Logged in as ${user.email}</h6>
-        <h6>${doc.data().bio}</h6>
-        `;
-          accdetails.innerHTML = html;
-        });
     });
     loggedOutLinks.forEach((item) => {
       item.style.display = "none";
     });
+    //now get data
+    db.collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const html = `
+    <h6>Logged in as ${user.email}</h6>
+    <h6>${doc.data().bio}</h6>
+    `;
+        accdetails.innerHTML = html;
+      });
   } else {
     loggedInLinks.forEach((item) => {
       item.style.display = "none";
